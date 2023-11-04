d37675cedaa3e0a0366761c7730ece791166d7ec
bokazuya25 <bo.kazuya.25@gmail.com>
2023-11-05 04:49:03 +0900

    タブレット・スマホ用のレスポンシブデザインを作成した（ブレイクポイント:768px）

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('header')
-    <div action="/search" method="get" class="header__right">
+    <div class="header__right">
         <label class="select-box__label">
             <select name="area" class="select-box__item">
                 <option value="">All area</option>
@@ -33,6 +33,52 @@
             </label>
         </div>
     </div>
+
+    <div class="header__right--hidden">
+        <div class="search__icon">
+            <input id="drawer__input-search" class="drawer__hidden-search" type="checkbox">
+            <label for="drawer__input-search" class="drawer__open-search"><span></span></label>
+                <div class="overlay"></div>
+            <div class="search__content">
+                <form action="/" method="get"  class="search__form">
+                    <div class="search__select">
+                        <label class="select-box__label">
+                            <select name="area" class="select-box__item">
+                                <option value="">All area</option>
+                                @foreach ($areas as $area)
+                                    <option class="select-box__option" value="{{ $area->id }}"
+                                        {{ request('area') == $area->id ? 'selected' : '' }}>{{ $area->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </label>
+
+                        <label class="select-box__label">
+                            <select name="genre" class="select-box__item">
+                                <option value="">All genre</option>
+                                @foreach ($genres as $genre)
+                                    <option value="{{ $genre->id }}" {{ request('genre') == $genre->id ? 'selected' : ''}}>{{ $genre->name }}</option>
+                                @endforeach
+                            </select>
+                        </label>
+                    </div>
+
+                    <div class="search__text">
+                        <div class="search__item">
+                            <div class="search__item-button"></div>
+                            <label class="search__item-label">
+                                <input type="text" name="word" class="search__item-input" placeholder="Search ..." value="{{ request('word') }}">
+                            </label>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="form__button">検索</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @section('content')
@@ -75,10 +121,11 @@
                 </div>
             </div>
         @endforeach
-        <div class="shop__content dummy"></div>
-        <div class="shop__content dummy"></div>
-        <div class="shop__content dummy"></div>
-        <div class="shop__content dummy"></div>
+
+        @for ($i = 0; $i < 4; $i++)
+            <div class="shop__content dummy"></div>
+        @endfor
+
     </div>
     <script src="{{ asset('js/search.js') }}"></script>
 @endsection
